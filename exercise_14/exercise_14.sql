ce6e53ec888b86363ea038f8d22f618c35984c3c
kevinnail <fumalicious@gmail.com>
2022-11-09 16:35:45 -0800

    14th query entered, test passing

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,8 @@
 -- the average film length by category
 -- include the category name and avg length
+select category.name, avg(film.length)
+from film
+left join film_category on film.film_id = film_category.film_id
+left join category on film_category.category_id = category.category_id
+group by category.name
+limit 16
